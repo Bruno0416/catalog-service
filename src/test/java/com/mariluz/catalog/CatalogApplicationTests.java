12a4fe16d97782b60c8af4e914b8405bd85f270b
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-19 17:19:47 -0400

    test: correcciones para ApplicationTests

--- src/test/java/com/mariluz/catalog/CatalogApplicationTests.java
+++ src/test/java/com/mariluz/catalog/CatalogApplicationTests.java
@@ -3,7 +3,19 @@ package com.mariluz.catalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=password",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "JWT_SECRET=98PAzqkLUAElxtt+iNCg3GxPvqHI8ytzNfilW2S3RhQ=",
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+    }
+)
 class CatalogApplicationTests {
 
     @Test
